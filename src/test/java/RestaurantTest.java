b4bb87c7bc42b76274b48e717bf4df83a82664f0
mladdha123 <105233158+mladdha123@users.noreply.github.com>
2022-06-08 19:20:47 +0530

    Part3: Failing test case
    Part3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -8,17 +8,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class RestaurantTest {
     Restaurant restaurant;
     //REFACTOR ALL THE REPEATED LINES OF CODE
+    @BeforeEach
+    public void setup() {
+
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+    }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
+	String restaurantToBeSearched = "Amelie's cafe";
+        Restaurant returnedRestaurant= service.findRestaurantByName(restaurantToBeSearched);
+        assertEquals(restaurantToBeSearched,returnedRestaurant.getName());
+
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
+	String restaurantToBeSearched = "Coco's cafe";
+        assertThrows(restaurantNotFoundException.class,()-> {
+            service.findRestaurantByName(restaurantToBeSearched);
+        });
 
     }
 
